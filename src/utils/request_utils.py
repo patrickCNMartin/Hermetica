eaad05c7346f87763addea1a8995f458d18d1bee
Patrick CN Martin <pcnmartin@gmail.com>
2026-05-22 12:26:58 +0200

    run pre commit trial

--- src/utils/request_utils.py
+++ src/utils/request_utils.py
@@ -4,9 +4,11 @@
 import requests
 
 
-
-def get_protocol_list(base_url,headers,filter:str = "shared_with_user",search_key:str=" ")->dict:
+def get_protocol_list(
+    base_url, headers, filter: str = "shared_with_user", search_key: str = " "
+) -> dict:
+    """Get a list of protocols from the protocols.io API."""
     request_url = f"{base_url}/v3/protocols?filter={filter}&key={search_key}"
-    protocol_list = requests.get(url = request_url, headers=headers)
+    protocol_list = requests.get(url=request_url, headers=headers)
     protocol_list.raise_for_status()
     return protocol_list.json()
